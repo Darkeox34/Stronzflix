b659632e8c39214104709b89fb2f825bd405f18f
Bonfra04 <bonfract@gmail.com>
2024-11-10 15:16:41 +0100

    cleaner implementation of loading dialog

--- lib/dialogs/loading_dialog.dart
+++ lib/dialogs/loading_dialog.dart
@@ -4,26 +4,24 @@ import 'package:flutter/material.dart';
 
 class LoadingDialog {
     static Future<T> load<T>(BuildContext context, Future<T> Function() future) async {
-        return await showDialog(
+        showDialog(
             context: context,
             barrierDismissible: false,
-            builder: (context) => Dialog(
-                backgroundColor: Colors.transparent,
-                child: FutureBuilder(
-                    future: future(),
-                    builder: ((context, snapshot) {
-                        if(snapshot.connectionState == ConnectionState.done) {
-                            Navigator.of(context).pop(snapshot.data);
-                            return const SizedBox.shrink();
-                        }
-
-                        return const Center(
-                            child: CircularProgressIndicator(),
-                        );
-                    })
+            builder: (context) => const PopScope(
+                canPop: false,
+                child:  Dialog(
+                    backgroundColor: Colors.transparent,
+                    child: Center(
+                        child: CircularProgressIndicator(),
+                    )
                 )
             )
         );
+
+        T result = await future();
+        if(context.mounted)
+            Navigator.of(context).pop();
+        return result;
     }
 
     static Future<T> progress<T>(BuildContext context, Stream<double> Function() stream) async {
